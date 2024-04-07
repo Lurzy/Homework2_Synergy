813b4a3723a222cb342dc621b8c0281ced4c8ea6
Lurzy <purpleswtf@gmail.com>
2024-04-07 17:59:41 +0300

    Добавьте файлы проекта.

---
+++ Homework2_Synergy.py
@@ -0,0 +1,6 @@
+print("Введите ширину и длину прямоугольника:")
+a, b = map(int, input().split())
+square = a * b
+perimeter = (a+b)*2
+print("Площадь прямоугольника равна: ", square)
+print("Периметр прямоугольника равен: ", perimeter)
